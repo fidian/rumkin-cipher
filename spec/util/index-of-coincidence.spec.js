066ea2a5bd799f17e98be5c6d0314c2fb6a724b1
Tyler Akins <fidian@rumkin.com>
2022-06-25 16:10:18 -0500

    Bringing code to new standards

--- spec/util/index-of-coincidence.spec.js
+++ spec/util/index-of-coincidence.spec.js
@@ -1,33 +1,44 @@
 "use strict";
 
-var English, english, Español, español, indexOfCoincidence;
-
-English = require("../../lib/alphabet/english");
-Español = require("../../lib/alphabet/español");
-english = new English();
-español = new Español();
-indexOfCoincidence = require("../../lib/util/index-of-coincidence");
+const English = require("../../lib/alphabet/english");
+const Español = require("../../lib/alphabet/español");
+const english = new English();
+const español = new Español();
+const indexOfCoincidence = require("../../lib/util/index-of-coincidence");
+const Message = require("../../lib/util/message");
 
 describe("indexOfCoincidence", () => {
     it("works with an empty string", () => {
-        expect(indexOfCoincidence("", english)).toEqual(0);
+        expect(indexOfCoincidence(new Message(""), english)).toEqual(0);
     });
     it("still returns zero with a single character", () => {
-        expect(indexOfCoincidence("a", english)).toEqual(0);
+        expect(indexOfCoincidence(new Message("a"), english)).toEqual(0);
     });
     it("returns 1 when all letters are the same", () => {
-        expect(indexOfCoincidence("aa", english)).toEqual(26);
+        expect(indexOfCoincidence(new Message("aa"), english)).toEqual(26);
     });
     it("calculates correctly with a slightly harder example", () => {
-        expect(indexOfCoincidence("aaab", english)).toEqual(13);
+        expect(indexOfCoincidence(new Message("aaab"), english)).toEqual(13);
     });
     it("improves the score when more letters are used", () => {
-        expect(indexOfCoincidence("aaabaaabaaabaaab", english)).toEqual(15.6);
+        expect(
+            indexOfCoincidence(new Message("aaabaaabaaabaaab"), english)
+        ).toEqual(15.6);
     });
     it("calculates correctly for a sentence", () => {
-        expect(indexOfCoincidence("MARYHADALITTLELAMBITSFLEECEASWHITEASSNOW", english)).toBeCloseTo(1.63333, 5);
+        expect(
+            indexOfCoincidence(
+                new Message("MARYHADALITTLELAMBITSFLEECEASWHITEASSNOW"),
+                english
+            )
+        ).toBeCloseTo(1.63333, 5);
     });
     it("shows a higher value when the language has more letters", () => {
-        expect(indexOfCoincidence("MARYHADALITTLELAMBITSFLEECEASWHITEASSNOW", español)).toBeCloseTo(1.69615, 5);
+        expect(
+            indexOfCoincidence(
+                new Message("MARYHADALITTLELAMBITSFLEECEASWHITEASSNOW"),
+                español
+            )
+        ).toBeCloseTo(1.69615, 5);
     });
 });
