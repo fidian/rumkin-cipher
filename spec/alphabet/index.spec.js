3547109123ff2fe2f080a4726c079e9a67286109
Tyler Akins <fidian@rumkin.com>
2022-08-04 09:24:38 -0500

    Upgrading packages

--- spec/alphabet/index.spec.js
+++ spec/alphabet/index.spec.js
@@ -15,9 +15,8 @@ describe("Alphabets", () => {
             });
         });
         describe(`${name} instance`, () => {
-            var instance;
+            const instance = new Alphabet();
 
-            instance = new Alphabet();
             it("has a name", () => {
                 expect(instance.name).not.toBe("Alphabet");
             });
@@ -25,29 +24,42 @@ describe("Alphabets", () => {
                 expect(instance.length).toBeGreaterThan(0);
             });
             it("has a length that matches the lower length", () => {
-                expect(instance.length).toEqual(instance.letterOrder.lower.length);
+                expect(instance.length).toEqual(
+                    instance.letterOrder.lower.length
+                );
             });
             it("has a length that matches the upper length", () => {
-                expect(instance.length).toEqual(instance.letterOrder.upper.length);
+                expect(instance.length).toEqual(
+                    instance.letterOrder.upper.length
+                );
             });
             it("converts the third lowercase letter to the right index", () => {
-                expect(instance.toIndex(instance.letterOrder.lower.charAt(2))).toBe(2);
+                expect(
+                    instance.toIndex(instance.letterOrder.lower.charAt(2))
+                ).toBe(2);
             });
             it("converts the third index to an uppercase letter", () => {
-                expect(instance.toLetter(2)).toBe(instance.letterOrder.upper.charAt(2));
+                expect(instance.toLetter(2)).toBe(
+                    instance.letterOrder.upper.charAt(2)
+                );
             });
-            describe("translations", () => {
-                Object.keys(instance.translations).forEach((input) => {
-                    it(`maps ${input} to base letters`, () => {
-                        var translated;
 
-                        translated = instance.translateString(input);
-                        instance.findLetterIndexes(translated).forEach((index) => {
-                            expect(index).not.toBe(-1);
+            if (Object.keys(instance.translations).length) {
+                describe("translations", () => {
+                    Object.keys(instance.translations).forEach((input) => {
+                        it(`maps ${input} to base letters`, () => {
+                            var translated;
+
+                            translated = instance.translateString(input);
+                            instance
+                                .findLetterIndexes(translated)
+                                .forEach((index) => {
+                                    expect(index).not.toBe(-1);
+                                });
                         });
                     });
                 });
-            });
+            }
         });
     });
 });
